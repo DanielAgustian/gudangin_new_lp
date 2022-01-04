f7b25c26393a63d3853e95ff4016a34b48ef9c1c
ezasyq <eza.yokesen@gmail.com>
2022-01-04 22:45:28 +0700

    connect register to backend done

--- resources/views/pages/auth/register.blade.php
+++ resources/views/pages/auth/register.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
     <meta name="description"
         content="Gudangin urus semua orderan tokomu siap kirim dalam waktu 5 menit, jualan lebih banyak, kirim lebih cepat kemanapun, tetap lebih hemat." />
     <meta name="keywords"
@@ -89,15 +89,18 @@
                         <img src="{{ asset('images/navbar_logo.svg') }}" alt="" class="register__logo">
                         <h3>Register</h3>
 
-                        {{-- <form action=""> --}}
-
                         <div class="form-group">
                             <label>Nomor Whatsapp</label>
                             <div class="input-group">
                                 <div class="input-group-prepend"><span class="input-group-text">+62</span></div>
-                                <input type="text" class="form-control" name="whatsapp"
-                                    placeholder="Silahkan masukkan Nomor Whatsapp Anda" />
+                                <input id="whatsapp" type="text" class="form-control" name="whatsapp"
+                                    placeholder="Silahkan masukkan Nomor Whatsapp Anda"
+                                    onkeypress="clearError('errWa')" />
                             </div>
+                            <span id="errWa" class="d-none" style="font-size: 12px; color: #FF0000">Nomor
+                                Whatsapp Harus
+                                diisi dengan format yang
+                                benar</span>
                         </div>
 
                         <div class="form-group">
@@ -106,9 +109,14 @@
                                 <div class="input-group-prepend"><span class="input-group-text">
                                         <img src="{{ asset('images/icons/email.svg') }}" alt="">
                                     </span></div>
-                                <input type="text" class="form-control" name="whatsapp"
-                                    placeholder="Silahkan masukkan Alamat Email Anda" />
+                                <input type="email" class="form-control" name="email" id="userEmail"
+                                    placeholder="Silahkan masukkan Alamat Email Anda"
+                                    onkeypress="clearError('errEmail')" />
                             </div>
+                            <span id="errEmail" class="d-none" style="font-size: 12px; color: #FF0000">Email
+                                Harus
+                                diisi dengan format yang
+                                benar</span>
                         </div>
 
                         <div class="form-group">
@@ -118,14 +126,18 @@
                                         <img src="{{ asset('images/icons/otp.svg') }}" alt="">
                                     </span></div>
                                 <input type="text" class="form-control" name="whatsapp" placeholder="Email Kode OTP"
-                                    style="border-right: transparent" />
+                                    style="border-right: transparent" id="kodeOTP" onkeypress="clearError('errOTP')" />
                                 <div class="input-group-append">
                                     <span class="input-group-text cursor-pointer"
-                                        style="border-left: transparent; color: #0027F4; cursor: pointer">
+                                        style="border-left: transparent; color: #0027F4; cursor: pointer"
+                                        onclick="sendOTP()">
                                         Kirim
                                     </span>
                                 </div>
                             </div>
+                            <span id="errOTP" class="d-none" style="font-size: 12px; color: #FF0000">Masukkan
+                                Kode
+                                OTP</span>
                         </div>
 
                         <div class="form-group">
@@ -134,41 +146,35 @@
                                 <div class="input-group-prepend"><span class="input-group-text">
                                         <img src="{{ asset('images/icons/password.svg') }}" alt="">
                                     </span></div>
-                                <input type="text" class="form-control" name="whatsapp" placeholder="Password"
-                                    style="border-right: transparent" />
+                                <input id="input-password" type="password" class="form-control" name="whatsapp"
+                                    placeholder="Password" style="border-right: transparent"
+                                    onkeypress="clearError('errPass')" />
                                 <div class="input-group-append">
                                     <span class="input-group-text cursor-pointer"
-                                        style="border-left: transparent; color: #B8B8B8; cursor: pointer">
+                                        style="border-left: transparent; color: #B8B8B8; cursor: pointer"
+                                        id="togglePassword" onclick="togglePassword('input-password')">
                                         <i class="fas fa-eye-slash"></i>
                                     </span>
                                 </div>
                             </div>
+                            <span id="errPass" class="d-none" style="font-size: 12px; color: #FF0000">Masukkan
+                                Kata
+                                Sandi</span>
                         </div>
-                        {{-- <div class="form-group">
-                            <label>Kata Sandi</label>
-                            <div class="input-group">
-                                <div class="input-group-prepend"><span class="input-group-text">
-                                        <img src="{{ asset('images/icons/password.svg') }}" alt="">
-                                    </span></div>
-                                <input type="text" class="form-control" name="whatsapp"
-                                    placeholder="Silahkan masukkan Kata Sandi Anda" />
-                            </div>
-                        </div> --}}
-
-                        {{-- </form> --}}
 
                         <div class="register__syarat">
                             <div class="form-check">
-                                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault"
+                                <input class="form-check-input" type="checkbox" value="" id="ckBox"
                                     style="height: unset; margin-top: 0.2rem">
-                                <label class="form-check-label" for="flexCheckDefault">
+                                <label class="form-check-label" for="ckBox">
                                     Dengan mendaftar, Saya setuju dengan <a href="">terms and conditions</a> dan <a
                                         href="">privacy policy Gudangin</a>
                                 </label>
                             </div>
                         </div>
 
-                        <button class="btn">Register</button>
+                        <button class="btn" disabled id="registerBtn"
+                            onclick="postRegister()">Register</button>
 
                         <div class="text-center my-3">
                             <p>Sudah Punya Akun? <a href="">Log In</a></p>
@@ -182,14 +188,106 @@
 
 
     <!-- Javascripts -->
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
-    </script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous">
     </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"
+        integrity="sha512-pHVGpX7F/27yZ0ISY+VVjyULApbDlD0/X0rgGbTqCE7WFW5MezNTWG/dnhtbBuICzsd0WQPgpE4REBLv+UqChw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
-    @yield('pageJS')
+    <script>
+        $(document).ready(function() {
+            $('#whatsapp').mask('000-0000-00000');
+            $('#kodeOTP').mask('000000');
+        });
+
+        function togglePassword(elmID) {
+            let password = document.getElementById(elmID);
+            if (password.type === "password") {
+                password.type = "text";
+                $(".fas").removeClass(" fa-eye-slash")
+                $(".fas").addClass("fa-eye")
+            } else {
+                password.type = "password";
+                $(".fas").removeClass("fa-eye")
+                $(".fas").addClass("fa-eye-slash")
+            }
+        }
+
+        $('#ckBox').click(function() {
+            $('#registerBtn').prop('disabled', !$(this).prop('checked'));
+        });
+    </script>
+
+    <script>
+        const sendOTP = () => {
+            let email = document.getElementById('userEmail').value
+            $.ajax({
+                url: `{{ env('API_URL') }}send-otp`,
+                headers: {
+                    'X-CSRF-TOKEN': '<?php echo csrf_token(); ?>',
+                    'Authorization': "Bearer {{ Session::get('token') }}",
+                    'Jwt-Key': '{{ env('API_KEY') }}'
+                },
+                data: {
+                    email: email
+                },
+                type: 'post',
+                success: function(data) {
+                    console.log(data)
+                    if (data.status == 'success') {
+                        console.log('berhasil gan')
+                    }
+                }
+            })
+        }
+
+        const postRegister = () => {
+            let whatsapp = document.getElementById('whatsapp').value
+            let email = document.getElementById('userEmail').value
+            let otp = document.getElementById('kodeOTP').value
+            let password = document.getElementById('input-password').value
+
+            if (whatsapp == "" || email == "" || otp == "" || password == "") {
+                whatsapp == "" ? $('#errWa').removeClass('d-none') : ""
+                email == "" ? $('#errEmail').removeClass('d-none') : ""
+                otp == "" ? $('#errOTP').removeClass('d-none') : ""
+                password == "" ? $('#errPass').removeClass('d-none') : ""
+            } else {
+                $.ajax({
+                    url: `{{ env('API_URL') }}register`,
+                    headers: {
+                        'X-CSRF-TOKEN': '<?php echo csrf_token(); ?>',
+                        'Authorization': "Bearer {{ Session::get('token') }}",
+                        'Jwt-Key': '{{ env('API_KEY') }}'
+                    },
+                    data: {
+                        email: email,
+                        password: password,
+                        password_confirmation: password,
+                        whatsapp: whatsapp,
+                        otp: otp
+                    },
+                    type: 'post',
+                    success: function(data) {
+                        console.log(data)
+                        if (data.status == 'success') {
+                            console.log('berhasil gan')
+                        }
+                    },
+                    error: function(err) {
+                        console.log(err)
+                    }
+                })
+            }
+        }
+
+        const clearError = (id) => {
+            $(`#${id}`).addClass('d-none')
+        }
+    </script>
 </body>
 
 
